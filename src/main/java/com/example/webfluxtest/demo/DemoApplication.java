1e08a9f3140ad1d0aeff401d1ca7a4209dcbb5f6
yeseric@163.com <yesEric001>
2019-01-17 13:29:39 +0800

    update

--- src/main/java/com/example/webfluxtest/demo/DemoApplication.java
+++ src/main/java/com/example/webfluxtest/demo/DemoApplication.java
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 @SpringBootApplication
 @Configuration
 @ComponentScan("com.example.webfluxtest.demo")
 @EnableWebFlux
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
+        log.debug("Server is Running");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
